2fdb5987da58cbeb54b89b37bba0ba9ad1ff1520
Yaa-Asantewaa <yaaasantewaadankwa@gmail.com>
2025-12-10 16:15:51 +0000

    text

---
+++ assignment.py
@@ -0,0 +1,23 @@
+#1
+playlist = ["lonely","stars","diamond","flying","grief"]
+
+#2
+top_artists = ("Akon","Sia","Nicki","Momo")
+
+#3
+my_music = [playlist,top_artists]
+
+#4
+print(len(my_music))
+
+#5
+print(playlist[2])
+
+#6
+playlist[0] = "Perfect"
+
+#7
+playlist.append("Blinding Lights")
+
+#8
+print(playlist)
