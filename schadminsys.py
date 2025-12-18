79045bbf3cad1ed2eabb2713023038fd7caa62fa
Yaa-Asantewaaa <yaaasantewaadankwa@gmail.com>
2025-12-18 15:47:55 +0000

    Add files via upload

---
+++ schadminsys.py
@@ -0,0 +1,10 @@
+def student_info(name, age, email, grade):
+    profile = {
+        "name": name,
+        "age": age,
+        "email": email,
+        "grade": grade
+    }
+    return profile
+
+
